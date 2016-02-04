3b04d84d4b309aa52b8c54b7b472627b4a2489f8
--global <--global>
2016-02-04 23:10:01 +0000

    Login styling

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,29 +1,39 @@
 <!-- Innerpages Content -->
-<section class="innerpage">
-    <!-- Login -->
-    <div class="login-wrap">
-        <div class="login-page signup">
-            <div class="row padding">
-                <div class="medium-6 small-12 div-center">
-                    <h2 class="inner-title text-center">Login</h2>
-                    <?php echo form_open("auth/login");?>
-                      <p>
-                            <?php echo lang('login_identity_label', 'identity');?>
-                            <?php echo form_input($identity);?>
-                      </p>
-                      <p>
-                          <?php echo lang('login_password_label', 'password');?>
-                          <?php echo form_input($password);?>
-                      </p>
-                      <p>
-                          <?php echo lang('login_remember_label', 'remember');?>
-                          <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-                      </p>
-                      <div class='div-center'<p><?php echo form_submit('submit', 'Login', array("class" => 'btn btn-contest'));?></p>
-                      <?php echo form_close();?>
-                      <p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+        <!-- Login -->
+        <div class="login-wrap">
+            <div class="login-page">
+                <div class="row padding">
+                    <div class="medium-6 small-12 div-center">
+                       <div class="medium-6 small-12 div-center">
+                         <?php echo form_open("auth/login");?>
+                          <h2 class="inner-title text-center">Login</h2>
+                            <div class="form-row">
+                                  <?php echo form_input(array('name' => 'email','value' => '','placeholder' => 'Email', 'type' => 'text'));?>
+                            </div>
+                            <div class="form-row">
+                                <?php echo form_input(array('name' => 'password', 'value' => '', 'placeholder' => 'Password', 'type' => 'password'));?>
+                            </div>
+                            <div class="form-row">
+                                <?php echo lang('login_remember_label', 'remember');?>
+                                <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
+                            </div>
+                            <div class='form-row'><?php echo form_submit('submit', 'Login', array("class" => 'btn'));?></div>
+                            <?php echo form_close();?>
+                            <p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+
+                             <div class="button-box">
+                                <a href="#0">
+                                    <img src="<?php echo base_url().'public/img/img-facebook.png' ?>">
+                                </a>
+                            </div>
+                            <div class="button-box">
+                                <a href="#0">
+                                    <img src="<?php echo base_url().'public/img/img-google.png' ?>">
+                                </a>
+                            </div>
+                      </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+
--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -5,6 +5,7 @@
         <meta charset="utf-8" />
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href='https://fonts.googleapis.com/css?family=Lato:400,300' rel='stylesheet' type='text/css'>
         <link href="<?php echo base_url().'public/css/foundation.css' ?>" rel="stylesheet">
 		<link href="<?php echo base_url().'public/css/app.css' ?>" rel="stylesheet">
 		<link href="<?php echo base_url().'public/css/slick.css' ?>" rel="stylesheet">
