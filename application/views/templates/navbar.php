d9458694cbf0388d9395a6a055d4f10761ae7177
--global <--global>
2016-02-04 17:07:57 +0000

    JS assets

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -9,6 +9,9 @@
 		<link href="public/css/app.css" rel="stylesheet">
 		<link href="public/css/slick.css" rel="stylesheet">
 		<link href="public/css/jcf.css" rel="stylesheet">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<script src='public/js/custom-select.js' type='text/javascript'></script>
+		<script src='public/js/foundation.min.js' type='text/javascript'></script>
 	</head>
 
 	<header>
@@ -16,7 +19,9 @@
 	        <div class="column medium-12">
 	            <div class="columns small-3 medium-1">
 	                <div class="logo">
-	                    <a href="#"><img src='public/img/logo.png' width='68' height='33'></a>
+	                    <a href="<?php echo base_url().'users/index'; ?>">
+	                    	<img src='public/img/logo.png' width='68' height='33'>
+	                    </a>
 	                </div>
 	            </div>
 	            <div class="columns small-9 medium-11">
@@ -27,18 +32,17 @@
 	                </div>
 	                <nav>
 	                    <ul>
-	                        <li><a href=''>DASHBOARD</a></li>
-	                        <li><a href=''>HOME</a></li>	                        
-	                        <li><a href=''>CONTESTS</a></li>	                        
-	                        <li><a href=''>LAUNCH CONTEST</a></li>	                        
-	                        <li><a href=''>HOW IT WORKS</a></li>	                        
-	                        <li><a href=''>CONTACT US</a></li>
+	                        <li><a href=''>Home</a></li>	                        
+	                        <li><a href=''>Contests</a></li>	                        
+	                        <li><a href=''>Launch</a></li>	                        
+	                        <li><a href=''>How it Works</a></li>	                        
+	                        <li><a href=''>Contact Us</a></li>
 	                        <li><a href=''>FAQS</a></li>	                        
                             <li class="login-li">
                                 <div class="login-box">
                                     <ul class="dropdown menu" data-dropdown-menu>
                                         <li>
-                                            <a href="#0" class="username">Name</a>
+                                            <a href="#0" class="username">N</a>
                                             <ul class="menu">
                                                 <li><a href=''>Profile</a></li>
                                                 <li><a href=''>Log out</a></li>
