de50f5a6797125aab98a943aadb2a3066014d1cd
--global <--global>
2016-02-05 23:01:04 +0000

    contents index

--- application/views/contests/index.php
+++ application/views/contests/index.php
@@ -43,20 +43,23 @@
                         <div class="medium-3 small-12 columns end">
                             <div class="submission-box">
                                 <div class="ad-company-info">
-                                    <a href="#"><?php echo $contest->company_name; ?></a>
+                                    <h4 class='text-center'><?php echo $contest->company_name; ?></a>
                                     <br>
-                                    <a href="#"><?php echo $contest->title; ?></a>
+                                    <h4 class='text-center'><?php echo $contest->title; ?></a>
                                     <div class="company-img">
 
                                     </div>
-                                    <h4>$50</h4>
-                                    <div class="progress-box2 fb-progress">
+                                    <h4 class='text-center'>$50 Reward</h4>
+                                    <div style='width:100%'>
                                         <div class="progress" role="progressbar" tabindex="0" aria-valuenow="20" aria-valuemin="0" aria-valuetext="25 percent" aria-valuemax="100">
                                             <span class="progress-meter" style="width:<?php echo $contest->submission_count; ?>%"></span>
                                         </div>
                                         <p class="progress-meter-text"><?php echo $contest->submission_count; ?> of 50 submissions</p>
                                     </div>
-                                    <h5>Ends <?php echo date('D, M d', strtotime($contest->stop_time));?> </h5>
+                                    <div><h5 class='text-center'>Ends <?php echo date('D, M d', strtotime($contest->stop_time));?></h5></div>
+                                    <?php if($contest->submission_count < 50) : ?>
+                                            <div class='text-center'><a href="<?php echo base_url().'contests/'.$contest->id ?>" style='cursor:pointer;text-decoration:none;' class='btn tiny'>tappyn</a></div>
+                                    <?php endif ?>
                                 </div>
                             </div>
                         </div>
