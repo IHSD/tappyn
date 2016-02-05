f7c17628e8543d4482ea783d137d8a8862909128
--global <--global>
2016-02-05 17:54:06 +0000

    Contact form

--- application/views/home/contact_us.php
+++ application/views/home/contact_us.php
@@ -1,9 +1,23 @@
-<!-- Innerpages Content -->
-    <section class="innerpage">
+
         <!-- Contact Us -->
         <div class="contact-us">
             <div class="row padding">
-                <h2 class="inner-title text-center">Contact Us</h2>
+                <div class="medium-6 small-12 div-center">
+                    <h2 class="inner-title text-center">Contact Us</h2>
+                    <?php echo form_open("welcome/contact");?>
+                    <div class="form-row">
+                          <?php echo form_dropdown('contact', array('' => 'Are you a customer or a creator?','company' => 'Customer','user' => 'Creator'), '');;?>
+                    </div>
+                    <div class="form-row">
+                        <?php echo form_input(array('name' => 'email', 'value' => '', 'placeholder' => 'Please enter your email', 'type' => 'text'));?>
+                    </div>
+                    <div class="form-row">
+                        <?php echo form_textarea(array('name' => 'details', 'value' => '', 'placeholder' => 'Please enter the details of your request. A member of our support staff will respond as soon as possible.', 'type' => 'text')); ?>
+                    </div>
+                    <div class='form-row'><?php echo form_submit('submit', 'Submit', array("class" => 'btn'));?></div>
+                    <?php echo form_close();?>
+                </div>
+                <!--
                 <div class="medium-8 div-center">
                     <div class="form-row">
                         <div class="medium-5 column">
@@ -44,7 +58,6 @@
                             <button class="btn">SUBMIT</button>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
-    </section>
