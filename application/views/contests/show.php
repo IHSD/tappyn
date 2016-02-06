22dd13a17b9e443bdc9f780749ad562e999e1ff4
--global <--global>
2016-02-06 02:51:17 +0000

    sep views

--- application/views/contests/show.php
+++ application/views/contests/show.php
@@ -2,7 +2,6 @@
 
 <?php defined("BASEPATH") or exit('No direct script access allowed'); ?>
 
-<?php echo $this->session->flashdata('error') ? $this->session->flashdata('error') : ''; ?>
 <div class='row'>	
 	<div class='medium-3 small-12 columns'>
 		<div class='contest-box'>
@@ -21,29 +20,12 @@
             </div>
             <div><h4 class='text-center'>Ends <?php echo date('D, M d', strtotime($contest->stop_time));?></h4></div>
             <?php if($contest->submission_count < 50) : ?>
-                <div class='text-center'><button id='submit_it' style='width:100%;cursor:pointer;text-decoration:none;' class='btn tiny'>Submit Now!</button></div>
+                <div class='text-center'><a href="<?php echo base_url().'contests/'.$contest->id.'/submissions' ?>" style='width:100%;cursor:pointer;text-decoration:none;' class='btn tiny'>View submissions</a></div>
             <?php endif ?>
 		</div>
 	</div>
-	<div id='submissions' class='medium-9 small-12 columns'>
-		<div class='contest-box'>	
-			<?php if(isset($submissions) && count($submissions) > 0) : ?>
-				<h3>Other submissions</h3>
-            	<?php foreach($submissions as $submission): ?>
-            		<div class="medium-3 small-12 columns end">
-            			<div>
-            				Hi
-            			</div>
-            		</div>
-            	<?php endforeach; ?>
-			<?php else : ?>
-				<h3>This contest has no submissions yet, you could be the first!</h3>
-			<?php endif ?>
-		</div>
-	</div>
-	<div id='submitting' class='hidden_submission medium-9 small-12 columns'>
+	<div id='submitting' class='medium-9 small-12 columns'>
 		<div class='contest-box'>
-			<button id='cancel_it' class='btn'>X</button>
 			<div class='row'>
 				<div class='medium-6 small-12 columns'>
 				<?php $this->load->view('templates/notification', array(
@@ -74,17 +56,3 @@
 
 
 <?php $this->load->view('templates/footer'); ?>
-
-<script>
-	$("#submit_it").click(function(){
-		$("#submit_it").addClass("hidden_submission");
-		$("#submissions").addClass("hidden_submission");
-		$("#submitting").removeClass('hidden_submission');
-	});
-	$("#cancel_it").click(function(){
-		$("#submissions").removeClass("hidden_submission");
-		$("#submit_it").removeClass("hidden_submission");
-		$("#submitting").addClass('hidden_submission');
-	});
-
-</script>
