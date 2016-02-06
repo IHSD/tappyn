57b08789c1a49cb69f74fa646f22238a53e127c2
Rob Wittman <rob@ihsdigital.com>
2016-02-05 20:22:16 -0500

    Added vendor javascript

---
+++ public/js/vendor/lucky_orange.js
@@ -0,0 +1,7 @@
+window.__lo_site_id = 50902;
+
+(function() {
+	var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
+	wa.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://cdn') + '.luckyorange.com/w.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
+})();
