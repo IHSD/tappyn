50f9acb088ed2cffaacd302fa48f389dfca0e38a
--global <--global>
2016-02-08 17:40:20 +0000

    twitter template

--- application/views/submissions/templates/twitter.php
+++ application/views/submissions/templates/twitter.php
@@ -8,8 +8,15 @@
         </div>
         <div class='twitter-input-wrapper'>
             <div class='form-row'>
-               <?php echo form_textarea(array('name' => 'text','value' => '','placeholder' => ($contest->objective == 'app_installs' ? "Use wit & humor to capture what makes this app unique" : ($contest->objective == 'website_clicks' ? "Use wit & humor to capture what makes this business unique" : ($contest->objective == 'engagement' ? "Create compelling content this business could supply" : "Create a captivating tweet"))), 'type' => 'text'));?>
+               <?php echo form_textarea(
+               array('name' => 'text','value' => '',
+               'placeholder' => 
+                    ($contest->objective == 'app_installs' ? "Use wit & humor to capture what makes this app unique" : 
+                    ($contest->objective == 'website_clicks' ? "Use wit & humor to capture what makes this business unique" : 
+                    ($contest->objective == 'engagement' ? "Create compelling content this business could supply" :
+                    "Create a captivating tweet"))), 'type' => 'text'. 'id' => 'text', 'rows' : '2'));?>
             </div>
+            <div class='input-count'><span id='text_span'>0</span> of 140</div>
             <a href='#' class='twitter-button'></a>
         </div>
     </div>
@@ -40,3 +47,10 @@
     font-size:0.7em
 }
 </style>
+<script>
+
+$('#text').keyup(function(){
+    var wordlength = $('#text').val().length;
+    $("#text_span").html(wordlength);
+});
+</script>
