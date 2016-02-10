da135c152b32837943d5a6a92a4049597213de93
Rob Wittman <rob@ihsdigital.com>
2016-02-10 11:56:56 -0500

    Fixed function name

--- application/libraries/Mailer.php
+++ application/libraries/Mailer.php
@@ -3,12 +3,18 @@
 class Mailer
 {
     protected $api_key;
+    protected $to;
+    protected $from;
+    protected $subject;
+    protected $html;
+    protected $callback;
 
     public function __construct()
     {
         $this->config->load('secrets');
         $api_key = $this->config->item('sendgrid_api_key');
         $this->handler = new SendGrid($api_key);
+        $this->email = new SendGrid\Email();
     }
 
     public function __get($var)
@@ -16,14 +22,37 @@ class Mailer
         return get_instance()->$var;
     }
 
-    public function test()
+    public function to($to)
     {
-        $email = new SendGrid\Email();
-        $email
-            ->addTo('rob@ihsdigital.com')
-            ->setFrom('squad@tappyn.com')
-            ->setSubject('Test email from tappyn.com')
-            ->setText('HelloWorld');
-        $this->handler->send($email);
+        $this->to = $to;
+        return $this;
+    }
+
+    public function from($from)
+    {
+        $this->from = $from;
+        return $this;
+    }
+
+    public function subject($subject)
+    {
+        $this->subject = $subject;
+        return $this;
+    }
+
+    public function html($html)
+    {
+        $this->html = $html;
+        return $this;
+    }
+
+    public function send()
+    {
+        $this->email
+            ->addTo($this->to)
+            ->setFrom($this->from)
+            ->setSubject($this->subject)
+            ->setHtml($this->html);
+        $this->handler->send($this->email);
     }
 }
