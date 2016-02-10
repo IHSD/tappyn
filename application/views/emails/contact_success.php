0bd2e2af6c498ee3680b61b8c82aa261f953729d
Rob Wittman <rob@ihsdigital.com>
2016-02-10 12:22:47 -0500

    Updated contact email

--- application/views/emails/contact_success.php
+++ application/views/emails/contact_success.php
@@ -1,7 +1,32 @@
 <?php defined("BASEPATH") or exit('No direct script access allowed'); ?>
 
-Customer : <?php echo $contact; ?>
 
-Email : <?php echo $email; ?>
+<table cellspacing="0" cellpadding="0" border="0">
+    <tr>
+        <td width="150">
+            Email
+        </td>
+        <td width='500'>
+            <?php echo $email; ?>
+        </td>
+    </tr>
+    <tr>
+        <td width="150">
+            Type
+        </td>
+        <td width='500'>
+            <?php echo $contact; ?>
+        </td>
+    </tr>
+    <tr>
+        <td width="150">
+            Message
+        </td>
+        <td width='500'>
+            <?php echo $details; ?>
+        </td>
+    </tr>
+    <tr>
 
-Message : <?php echo $details; ?>
+    </tr>
+</table>
