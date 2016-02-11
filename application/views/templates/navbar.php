578c5c75ecc63df3eb7de13d707ffae51ddf4837
Rob Wittman <rob@ihsdigital.com>
2016-02-11 18:35:21 -0500

    Added FB tracking pixels

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -24,6 +24,20 @@
 	</head>
 
 	<header>
+        <script>
+            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+            document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1019195258103569');
+            fbq('track', "PageView");
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=1019195258103569&ev=PageView&noscript=1"
+        /></noscript>
+<!-- End Facebook Pixel Code -->
             <div id="fb-root"></div>
             <script>(function(d, s, id) {
               var js, fjs = d.getElementsByTagName(s)[0];
