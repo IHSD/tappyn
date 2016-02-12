0ada7fb4704dfeda152231a78e411372f4203dcf
zharrington <herpderpofthederp@gmail.com>
2016-02-12 15:19:21 +0000

    Submissions page

--- application/vendor/composer/autoload_namespaces.php
+++ application/vendor/composer/autoload_namespaces.php
@@ -6,5 +6,9 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Smtpapi' => array($vendorDir . '/sendgrid/smtpapi/lib'),
+    'SendGrid' => array($vendorDir . '/sendgrid/sendgrid/lib'),
     'PayPal' => array($vendorDir . '/paypal/rest-api-sdk-php/lib'),
+    'Guzzle\\Tests' => array($vendorDir . '/guzzle/guzzle/tests'),
+    'Guzzle' => array($vendorDir . '/guzzle/guzzle/src'),
 );
