b9616922be6de13dc9567daf57e0aa0156c1e65c
Rob Wittman <rob@ihsdigital.com>
2016-02-15 16:31:48 -0500

    Modified winner email

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -13,12 +13,19 @@ class Test extends CI_Controller {
             'password' => bin2hex(openssl_random_pseudo_bytes(5)),
             'email' => 'rob@ihsdigital.com'
         );
-        // $this->mailer
-        //     ->to('rob@ihsdigital.com')
-        //     ->from('Registration@tappyn.com')
-        //     ->subject('Account successfully created')
-        //     ->html($this->load->view('emails/submission_chosen', $this->data, TRUE))
-        //     ->send();
+        $company_name = $this->db->select('name')->from('profiles')->where("id", 31)->get();
+        if($company_name)
+        {
+            $company_name = $company_name->row()->name;
+        } else {
+            $company_name = '';
+        }
+        $this->mailer
+            ->to('rob@ihsdigital.com')
+            ->from('Registration@tappyn.com')
+            ->subject('Account successfully created')
+            ->html($this->load->view('emails/submission_chosen', array('company_name' => $company_name), TRUE))
+            ->send();
         $this->load->view('emails/submission_chosen', $this->data);
     }
 }
