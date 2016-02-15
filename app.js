9969438e408bc03e6134216518e13ea119e55c23
zharrington <herpderpofthederp@gmail.com>
2016-02-15 22:01:55 +0000

    Progress

--- app.js
+++ app.js
@@ -1,2 +1,48 @@
-console.log('hi');
+var tappyn = angular.module('tappyn', [
+	'ngRoute',
+	'ui.bootstrap',
+	'ngAnimate'
+]);
+
+tappyn.config(function($routeProvider) {
+	$routeProvider
+	.when('/home', {
+		templateUrl : 'components/home/view.html',
+		controller : 'homeController'
+	})
+	.when('/login', {
+		templateUrl : 'components/login/view.html',
+		controller : 'loginController'
+	})
+	.otherwise({redirectTo : '/home'})
+
+});
+
+tappyn.controller("ApplicationController", function(AppFact){
+
+});
+
+tappyn.factory("AppFact", function($http){
+	var fact = {};
+	fact.logging_in = function(email, pass){
+		var object = {'identity' : email, 'password' : pass}; 
+		return $http({
+			method : 'POST',
+			url : 'index.php/login',
+			headers : {
+				'Content-type' : 'application/x-www-form-urlencoded'
+			}
+		});
+	}
+	return fact;
+})
 console.log("contestusals");
+tappyn.controller('homeController', function(){
+	
+})
+tappyn.controller('loginController', function(){
+	
+});
+tappyn.factory('loginFactory', function($http){
+	
+});
--- components/config.js
+++ components/config.js
@@ -1,1 +1,38 @@
-console.log('hi');
+var tappyn = angular.module('tappyn', [
+	'ngRoute',
+	'ui.bootstrap',
+	'ngAnimate'
+]);
+
+tappyn.config(function($routeProvider) {
+	$routeProvider
+	.when('/home', {
+		templateUrl : 'components/home/view.html',
+		controller : 'homeController'
+	})
+	.when('/login', {
+		templateUrl : 'components/login/view.html',
+		controller : 'loginController'
+	})
+	.otherwise({redirectTo : '/home'})
+
+});
+
+tappyn.controller("ApplicationController", function(AppFact){
+
+});
+
+tappyn.factory("AppFact", function($http){
+	var fact = {};
+	fact.logging_in = function(email, pass){
+		var object = {'identity' : email, 'password' : pass}; 
+		return $http({
+			method : 'POST',
+			url : 'index.php/login',
+			headers : {
+				'Content-type' : 'application/x-www-form-urlencoded'
+			}
+		});
+	}
+	return fact;
+})
