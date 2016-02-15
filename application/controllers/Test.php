4487bef0dd7172feb844492189b7b1cfa2a5f08d
Rob Wittman <rob@ihsdigital.com>
2016-02-15 16:27:44 -0500

    Edited submissions winner email

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -13,12 +13,12 @@ class Test extends CI_Controller {
             'password' => bin2hex(openssl_random_pseudo_bytes(5)),
             'email' => 'rob@ihsdigital.com'
         );
-        $this->mailer
-            ->to('rob@ihsdigital.com')
-            ->from('Registration@tappyn.com')
-            ->subject('Account successfully created')
-            ->html($this->load->view('auth/email/inline_registration', $this->data, TRUE))
-            ->send();
-        //$this->load->view('emails/submission_success', $this->data);
+        // $this->mailer
+        //     ->to('rob@ihsdigital.com')
+        //     ->from('Registration@tappyn.com')
+        //     ->subject('Account successfully created')
+        //     ->html($this->load->view('emails/submission_chosen', $this->data, TRUE))
+        //     ->send();
+        $this->load->view('emails/submission_chosen', $this->data);
     }
 }
