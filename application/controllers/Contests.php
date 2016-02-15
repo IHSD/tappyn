38aae326654f9e8590b0cab167a946a00a840b35
Rob Wittman <rob@ihsdigital.com>
2016-02-15 16:00:38 -0500

    Debugging

--- application/controllers/Contests.php
+++ application/controllers/Contests.php
@@ -7,6 +7,10 @@ class Contests extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        if($this->ion_auth->is_admin())
+        {
+            $this->load->view('templates/admin_navbar');
+        }
         $this->load->view('templates/navbar');
         $this->load->model('contest');
         $this->load->model('submission');
