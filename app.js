c728a1dabed218e63a16f3ceb247fe644ace7fff
zharrington <herpderpofthederp@gmail.com>
2016-02-22 20:58:31 +0000

    Payment testerino

--- app.js
+++ app.js
@@ -202,6 +202,26 @@ tappyn.factory("AppFact", function($http){
 	}
 	return fact;
 })
+tappyn.controller('contestsController', function($scope, contestsFactory){
+	contestsFactory.grabContests().success(function(response){
+		$scope.contests = response.data;
+	});
+})
+tappyn.factory('contestsFactory', function($http){
+	var fact = {};
+
+	fact.grabContests = function(){
+		return $http({
+			method : 'GET',
+			url : 'index.php/contests',
+			headers : {
+				'Content-type' : 'application/x-www-form-urlencoded'
+			}
+		});
+	}
+
+	return fact;
+})
 tappyn.controller('contestController', function($scope, $routeParams, $location, contestFactory){
 	contestFactory.grabContest($routeParams.id).success(function(response){
 		$scope.contest = response.data.contest;
@@ -249,26 +269,6 @@ tappyn.factory('contestFactory', function($http){
 			},
 			'data' : $.param(submission)
 		});	
-	}
-
-	return fact;
-})
-tappyn.controller('contestsController', function($scope, contestsFactory){
-	contestsFactory.grabContests().success(function(response){
-		$scope.contests = response.data;
-	});
-})
-tappyn.factory('contestsFactory', function($http){
-	var fact = {};
-
-	fact.grabContests = function(){
-		return $http({
-			method : 'GET',
-			url : 'index.php/contests',
-			headers : {
-				'Content-type' : 'application/x-www-form-urlencoded'
-			}
-		});
 	}
 
 	return fact;
@@ -393,13 +393,28 @@ tappyn.factory('launchFactory', function($http){
 	}
 	return fact;
 })
+tappyn.controller('profileController', function($scope){
+	
+});
+tappyn.factory('profileFactory', function($http){
+	var fact = {};
+
+	fact.grabProfile = function(){
+		
+	}
+
+	return fact;
+})
 tappyn.controller("paymentController", function($scope, $location, paymentFactory, paymentModel){
 	$scope.countries = paymentModel.countries;
 	$scope.showing = "methods";
 	paymentFactory.grabDetails().success(function(response){
 		if(response.http_status_code == 200){
 			if(response.success){
-				if($scope.user.type == "member" && response.data.account == false) $scope.showing = 'details';
+				if($scope.user.type == "member" && response.data.account == false){
+					$scope.detail = {first_name : $scope.user.first_name, last_name : $scope.user.last_name};
+					$scope.showing = 'details';
+				}
 				else $scope.showing = 'methods';
 			}
 			else $scope.set_alert(response.message, "default");	 
@@ -408,8 +423,57 @@ tappyn.controller("paymentController", function($scope, $location, paymentFactor
 		else $scope.check_code(response.http_status_code);
 	})
 
+	$scope.verify_identity = function(detail){
+		paymentFactory.verifyIdentity(detail).success(function(response){
+			if(response.http_status_code == 200){
+				if(response.success){
+					$scope.set_alert(response.message, "default");	
+					$scope.account = response.data;
+					$scope.showing = 'methods';
+				}
+				else $scope.set_alert(response.message, "default");	 
+			}
+			else if(response.http_status_code == 500) $scope.set_alert(response.error, "error");
+			else $scope.check_code(response.http_status_code);
+		});
+	}
+
 	$scope.toggle_view = function(view){
 		$scope.showing = view;
+	}
+
+	function stripeResponseHandler(status, response) {
+      var $form = $('#payment-form');
+
+      if (response.error) {
+        // Show the errors on the form
+        $form.find('.payment-errors').text(response.error.message);
+        $form.find('button').prop('disabled', false);
+      } else {
+        // response contains id and card, which contains additional card details
+        var token = response.id;
+        console.log(token);
+        return;
+        // Insert the token into the form so it gets submitted to the server
+        $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+        // and submit
+        $form.get(0).submit();
+      }
+    };
+	$scope.process_addition = function(ele){
+		// This identifies your website in the createToken call below
+		Stripe.setPublishableKey("pk_live_ipFoSG1UY45RGNkCpLVUaSBx");
+		var $form = $(ele);
+
+        // Disable the submit button to prevent repeated clicks
+        $form.find('button').prop('disabled', true);
+
+		Stripe.card.createToken($form, stripeResponseHandler);
+
+        // Prevent the form from submitting with the default action
+        return false;
+
+
 	}
 });
 tappyn.factory("paymentFactory", function($http){
@@ -425,6 +489,16 @@ tappyn.factory("paymentFactory", function($http){
 		})	
 	}
 
+	fact.verifyIdentity = function(details){
+		return $http({
+			method : 'POST',
+			url : 'index.php/accounts/details',
+			headers : {
+				'Content-type' : 'application/x-www-form-urlencoded'
+			},
+			data : $.param(details)
+		})	
+	}
 	return fact;	
 })
 tappyn.service('paymentModel', function(){
@@ -697,16 +771,4 @@ tappyn.factory("submissionsFactory", function($http){
 	}
 
 	return fact; 
-})
-tappyn.controller('profileController', function($scope){
-	
-});
-tappyn.factory('profileFactory', function($http){
-	var fact = {};
-
-	fact.grabProfile = function(){
-		
-	}
-
-	return fact;
 })
