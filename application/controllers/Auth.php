4873533ffeff58c94a578a478f77e43074240f4f
Robert Wittman <rob@ihsdigital.com>
2016-02-23 16:04:41 -0500

    Fixed some ish

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -37,9 +37,7 @@ class Auth extends CI_Controller {
 		$this->load->library('facebook_ion_auth');
 		if($this->input->get('submission'))
 		{
-			// Set submission data in the session, so we can create it when they get back
-			$submission_data = json_decode($this->input->get('submission'));
-			$submission_data = urldecode($submission_data);
+			$submission_data = json_decode(urldecode($this->input->get('submission'))));
 			if(is_null($submission_data)) die("Invalid data provided in submission object");
 			$this->session->set_flashdata('contest', $submission_data->contest);
 			$this->session->set_flashdata('text', $submission_data->text);
