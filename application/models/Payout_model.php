153b9defb419d624b284ca17cb2604a741a9f958
Robert Wittman <rob@ihsdigital.com>
2016-02-23 12:31:24 -0500

    Stashing

---
+++ application/models/Payout_model.php
@@ -0,0 +1,27 @@
+<?php defined("BASEPATH") or exit('No direct script access allowed');
+
+class Payout_model extends MY_Model
+{
+    protected $table = 'payouts';
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function count()
+    {
+        $this->db->select('COUNT(*) as count')->from($this->table);
+        if(!empty($this->where))
+        {
+            $this->db->where($this->where);
+        }
+        $count = $this->db->get();
+        if($count)
+        {
+            return $count->row()->count;
+        }
+        return FALSE;
+    }
+}
