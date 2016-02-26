ec60ed90562f7966a536ad439883577a19f3ee60
Robert Wittman <rob@ihsdigital.com>
2016-02-26 15:18:36 -0500

    Removed fusion charts

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -12,8 +12,6 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-        <script src="<?php echo base_url().'public/fusion_charts/js/fusioncharts.js' ?>"></script>
-        <script src="<?php echo base_url().'public/fusion_charts/js/fusioncharts.charts.js' ?>"></script>
 
     </head>
     <header>
