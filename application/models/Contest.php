0bc3eae61e6fa1824d924c1044f14a3e3b02d72a
Robert Wittman <rob@ihsdigital.com>
2016-03-01 15:49:01 -0500

    Debugigng

--- application/models/Contest.php
+++ application/models/Contest.php
@@ -125,7 +125,7 @@ class Contest extends MY_Model
             $this->messages = 'Contest successfully created';
             return $this->db->insert_id();
         }
-        $this->errors = $this->db->_error_message();
+        $this->errors = $this->db->error()['message'];
         return FALSE;
     }
 
