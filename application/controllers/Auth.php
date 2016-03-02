5e751db6259afe599bbe1752593eee0c6af30c0d
Robert Wittman <rob@ihsdigital.com>
2016-03-01 19:00:39 -0500

    Removed error logs and fixed redirects

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -48,7 +48,6 @@ class Auth extends CI_Controller {
 			$this->session->set_flashdata('headline', $submission_data->headline);
 			$this->session->set_flashdata('submitting_as_guest', 'true');
 		} else if($this->input->get('vote')) {
-			error_log("Setting voting as guest toggle to true");
 			$this->session->set_userdata('voting_as_guest', 'true');
 			$this->session->set_userdata('contest_id', $this->input->get('cid'));
 			$this->session->set_userdata('submission_id', $this->input->get('sid'));
@@ -68,7 +67,7 @@ class Auth extends CI_Controller {
 				{
 					$this->user->attribute_points($this->ion_auth->user()->row()->id, $this->config->item('points_per_submission'));
 					$this->session->set_flashdata('message', "Submission successfully created");
-					redirect('/#/dashboard', 'refresh');
+					redirect('#/contests', 'refresh');
 				}
 				else
 				{
