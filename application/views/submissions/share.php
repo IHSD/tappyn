35baec833640823855266b147c85bca29d8dd5fb
Robert Wittman <rob@ihsdigital.com>
2016-03-08 11:45:09 -0500

    Fixed tappyn logo in share dialog

--- application/views/submissions/share.php
+++ application/views/submissions/share.php
@@ -2,10 +2,15 @@
 <html>
     <head>
         <title>Tappyn</title>
-        <meta property="og:url" content="http://test.tappyn.com/submissions/share/<?php echo $submission->id; ?>">
+        <meta property="og:url" content="http://test.tappyn.com/#/submissions/share/<?php echo $submission->id; ?>">
         <meta property="og:type" content="article">
         <meta property="og:title" content="Test Title">
         <meta property="og:description" content="Test Description">
-        <meta property="og:image" content="https://tappyn.com/public/img/TappynLogo2/png">
+        <meta property="og:image:secure_url" content="https://tappyn.com/public/img/TappynLogo2.png">
     </head>
+    <body>
+        <script>
+            window.location.href("<?php echo base_url().'#/contest/'.$submission->contest_id; ?>");
+        </script>
+    </body>
 </html>
