32b9cf9902d7b20ed68e0270f3058ff3d663a495
Robert Wittman <rob@ihsdigital.com>
2016-03-08 15:54:58 -0500

    Statshing

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -10,4 +10,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 |	http://codeigniter.com/user_guide/general/hooks.html
 |
+|
+|   pre_system                  => Called very early during system execution. Only benchmarking and hooks have been loaded
+|   pre_controller              => Called immeditaely prior to any controllers being called.
+|   post_controller_constructor => Called immediately after controller is instantiated
+|   post_controller             => Called immediately after controller has finished executing
+|   display_override            => Overrides the _display() method, to send finalized page to web browser
+|   cache_override              => Enables you to call your own metho instead of the _display_cache() method
+|   post_system                 => Called after the final rendered page has been sent to the browser
 */
