e30a9e423f45a78a5544cb2277b3125f1614ae62
Robert Wittman <rob@ihsdigital.com>
2016-03-09 16:28:24 -0500

    Removed test function

--- application/controllers/Submissions.php
+++ application/controllers/Submissions.php
@@ -228,28 +228,4 @@ class Submissions extends CI_Controller
             $this->responder->fail("There was an error rating your submission")->code(500)->respond();
         }
     }
-
-    public function test()
-    {
-        $n1 = $_GET['n1'];
-        $n2 = $_GET['n2'];
-
-        Header ("Content-type: image/jpeg");
-        $image = imageCreateFromJPEG("images/someimage.jpg");
-        $color = ImageColorAllocate($image, 255, 255, 255);
-
-        // Calculate horizontal alignment for the names.
-        $BoundingBox1 = imagettfbbox(13, 0, 'ITCKRIST.TTF', $n1);
-        $boyX = ceil((125 - $BoundingBox1[2]) / 2); // lower left X coordinate for text
-        $BoundingBox2 = imagettfbbox(13, 0, 'ITCKRIST.TTF', $n2);
-        $girlX = ceil((107 - $BoundingBox2[2]) / 2); // lower left X coordinate for text
-
-        // Write names.
-        imagettftext($image, 13, 0, $boyX+25, 92, $color, 'ITCKRIST.TTF', $n1);
-        imagettftext($image, 13, 0, $girlX+310, 92, $color, 'ITCKRIST.TTF', $n2);
-
-        // Return output.
-        ImageJPEG($image, NULL, 93);
-        //ImageDestroy($image);
-    }
 }
