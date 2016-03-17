b2fc9e7f3cc1b8d4a0aae7be63232a3ff3bc8a35
Robert Wittman <rob@ihsdigital.com>
2016-03-17 13:53:19 -0400

    Started templates for PCP

--- application/views/emails/post_contest_package.php
+++ application/views/emails/post_contest_package.php
@@ -1,25 +1,4 @@
-<?php defined("BASEPATH") or exit('No direct script access allowed');
-
-/**
- * Our post contest package
- *
- */
-
-function parse_objective($obj)
-{
-    return $obj;
-}
-
-function parse_audience($aud)
-{
-    return '25-34 Females';
-}
-
-function parse_location($loc)
-{
-    return $loc;
-}
-?>
+<?php defined("BASEPATH") or exit('No direct script access allowed'); ?>
 
 
 <!-- Start Template -->
@@ -31,71 +10,21 @@ function parse_location($loc)
 <h2 style='text-align:left;margin:auto;width:600px;'>Hi <?php echo $cname; ?>,</h2>
 <br>
 
-<p style='text-align:center;margin:auto;width:600px'>Congratulations on picking your winning submission!</p><br>
+<p style='text-align:left;margin:auto;width:600px'>Congratulations on picking your winning submission!</p><br>
 
-<p style='text-align:center;margin:auto;width:600px;border-bottom:2px solid #FF5E00'></p>
-<br>
-<table style='max-width:500px' align='center'>
-    <?php if(isset($headline)): ?>
-    <tr>
-        <td>
-            <p style='max-width:600px;text-align:left;font-weight:700'>
-                <?php echo $headline; ?>
-            </p>
-        </td>
-    </tr>
-    <?php endif; ?>
-    <tr>
-        <td>
-            <p style='max-width:600px;text-align:justify;'>
-                <?php echo $text; ?>
-            </p>
-        </td>
-    </tr>
-</table>
-<br>
+<?php if(isset($headline)): ?>
+    <p style='text-align:left;margin:auto;width:600px'><strong>Headline</strong></p>
+    <p style='text-align:left;margin:auto;width:600px'><?php echo $headline; ?></p><br>
+<?php endif; ?>
+<?php if(isset($text)): ?>
+    <p style='text-align:left;margin:auto;width:600px'><strong>Text</strong></p>
+    <p style='text-align:left;margin:auto;width:600px'><?php echo $text; ?></p><br>
+<?php endif; ?>
 
-<p style='text-align:center;margin:auto;width:600px;border-bottom:2px solid #FF5E00'></p>
-<br>
+<p style='text-align:left;margin:auto;width:600px'><strong>Now it's time to put your content to work!</strong></p><br>
 
-<p style='text-align:center;margin:auto;width:600px'><strong>Now it's time to put your content to work!</strong></p><br>
-<?php
+<?php $this->load->view('emails/templates/'.$platform); ?>
 
-/**
- * Switch based on platform and contest objective
- */
-
-$audience_test = new StdClass;
-$audience_test->age_range = $age;
-$audience_test->gender = $gender;
-$data = array(
-    'objective_display_name' => parse_objective($objective),
-    'audience' => parse_audience($audience_test),
-    'location' => parse_location($location)
-);
-
-switch($platform) {
-    case 'facebook':
-        $this->load->view('emails/templates/facebook', $data);
-    break;
-
-    case 'google':
-        $this->load->view('emails/templates/facebook', $data);
-    break;
-
-    case 'twitter':
-        $this->load->view('emails/templates/facebook', $data);
-    break;
-
-    default:
-        $this->load->view('emails/templates/facebook', $data);
-}
-
-?>
-<p style='text-align:center;margin:auto;width:600px'>Unlesss it's peach vodka. Then keep it far, far away from our mailbox.</p><br><br>
-<p style='text-align:center;margin:auto;width:600px'>
-
-</p>
 <br>
 
 <!-- Begin footer -->
