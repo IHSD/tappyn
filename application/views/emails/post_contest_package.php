3a2887ea3414c5c15b5e07e1cbb4e716ef96980a
Robert Wittman <rob@ihsdigital.com>
2016-04-08 14:49:04 -0400

    Statshing

--- application/views/emails/post_contest_package.php
+++ application/views/emails/post_contest_package.php
@@ -10,26 +10,32 @@
 <h2 style='text-align:left;margin:auto;width:600px;'>Hi <?php echo $cname; ?>,</h2>
 <br>
 
-<p style='text-align:left;margin:auto;width:600px'>Congratulations on picking your winning submission!</p><br>
+<p style='text-align:left;margin:auto;width:600px'>Thanks for using Tappyn and congrats on picking an amazing ad!</p><br>
+<p style='text-align:left;margin:auto;width:600px'>Youre audience is going to love it.</p><br>
 
-<?php if(isset($headline)): ?>
-    <p style='text-align:left;margin:auto;width:600px'><strong>Headline</strong></p>
-    <p style='text-align:left;margin:auto;width:600px'><?php echo $headline; ?></p><br>
+<p style='text-align:left;margin:auto;width:600px'><strong><u>Details</u></strong></p>
+<p style='text-align:left;margin:auto;width:600px'><strong>Medium: </strong><?php echo ucfirst($contest->platform); ?></p>
+<p style='text-align:left;margin:auto;width:600px'><strong>Objecive: </strong><?php echo snake_to_string($contest->objective); ?></p>
+<p style='text-align:left;margin:auto;width:600px'><strong>Target Audience: </strong><?php echo $contest->min_age; ?> - <?php echo $contest->max_age; ?> year old<?php echo $contest->gender == 0 ? 's' : ($contest->gender == 1 ? 'Males' : 'Females'); ?> who like '<?php echo parse_interest($contest->industry); ?>'</p>
+<br>
+<p style='text-align:left;margin:auto;width:600px'><strong><u>Ad Creative</u></strong></p>
+<?php if(!is_null($submission->headline)): ?>
+    <p style='text-align:left;margin:auto;width:600px'><u><strong>Headline :</strong></u> <?php echo $submission->headline; ?>
 <?php endif; ?>
-<?php if(isset($text)): ?>
-    <p style='text-align:left;margin:auto;width:600px'><strong>Text</strong></p>
-    <p style='text-align:left;margin:auto;width:600px'><?php echo $text; ?></p><br>
+<?php if(!is_null($submission->text)): ?>
+    <p style='text-align:left;margin:auto;width:600px'><u><strong>Text :</strong></u> <?php echo $submission->text; ?>
 <?php endif; ?>
-
-<p style='text-align:left;margin:auto;width:600px'><strong>Now it's time to put your content to work!</strong></p><br>
-
-<?php $this->load->view('emails/templates/'.$platform); ?>
-
+<?php if(!is_null($submission->description)): ?>
+    <p style='text-align:left;margin:auto;width:600px'><u><strong>Description :</strong></u> <?php echo $submission->description; ?>
+<?php endif; ?>
+<?php if(!is_null($submission->link_explanation)): ?>
+    <p style='text-align:left;margin:auto;width:600px'><u><strong>Link Explanation :</strong></u> <?php echo $submission->link_explanation; ?>
+<?php endif; ?>
 <br>
 
 <!-- Begin footer -->
 <p style='margin:auto;width:600px;'>
-    Thanks
+    We hope to see you again soon!
 </p>
 <br>
 <p style='margin:auto;width:600px;'>
@@ -38,3 +44,50 @@
     <a href="<?php echo base_url(); ?>">www.tappyn.com</a>
 </p>
 <!-- End footer -->
+
+
+<?php
+
+function parse_interest($interest)
+{
+    switch($interest)
+    {
+        case 'travel':
+            return 'Travel';
+            break;
+        case 'food_beverage':
+            return 'Food and Beverage';
+            break;
+        case 'finance_business':
+            return 'Finance & Business';
+            break;
+        case 'health_wellness':
+            return 'Health & Wellness';
+            break;
+        case 'social_network':
+            return 'Social Network';
+            break;
+        case 'home_garden':
+            return 'Home & Garden';
+            break;
+        case 'education':
+            return 'Education';
+            break;
+        case 'art_entertainment':
+            return 'Art & Entertainment';
+            break;
+        case 'fashion_beauty':
+            return 'Fashion & Beauty';
+            break;
+        case 'tech_science':
+            return 'Tech & Science';
+            break;
+        case 'pets':
+            return 'Pets';
+            break;
+        case 'sports_outdoors':
+            return 'Sports & Outdoors';
+            break;
+        default: return $interest;
+    }
+}
