977ca524fd29373027cf4d6f5bc66b28eff88864
zharrington <herpderpofthederp@gmail.com>
2016-04-11 22:28:59 +0000

    following changes

--- components/comp_pro/controller.js
+++ components/comp_pro/controller.js
@@ -1,5 +1,45 @@
 tappyn.controller('comproController', function($scope, $rootScope, $routeParams, comproFactory){
 	if($routeParams.id){
-		
+		comproFactory.grabProfile($routeParams.id).success(function(response){
+			if(response.http_status_code == 200){
+				if(response.success) $scope.company = response.data;
+				else $scope.set_alert(response.message, "default");	 
+			}
+			else if(response.http_status_code == 500) $scope.set_alert(response.error, "error");
+			else $scope.check_code(response.http_status_code);
+		});	
+	}
+
+	$scope.follow = function(){
+		comproFactory.followCompany($routeParams.id).success(function(response){
+			if(response.http_status_code == 200){
+				if(response.success) $scope.set_alert(response.message, "default");	
+				else $scope.set_alert(response.message, "default");	 
+			}
+			else if(response.http_status_code == 500) $scope.set_alert(response.error, "error");
+			else $scope.check_code(response.http_status_code);
+		})
+	}
+
+	$scope.unfollow = function(){
+		comproFactory.unfollowCompany($routeParams.id).success(function(response){
+			if(response.http_status_code == 200){
+				if(response.success) $scope.set_alert(response.message, "default");	
+				else $scope.set_alert(response.message, "default");	 
+			}
+			else if(response.http_status_code == 500) $scope.set_alert(response.error, "error");
+			else $scope.check_code(response.http_status_code);
+		})
+	}
+
+	$scope.request_contest = function(){
+		comproFactory.requestContest($routeParams.id).success(function(response){
+			if(response.http_status_code == 200){
+				if(response.success) $scope.set_alert(response.message, "default");	
+				else $scope.set_alert(response.message, "default");	 
+			}
+			else if(response.http_status_code == 500) $scope.set_alert(response.error, "error");
+			else $scope.check_code(response.http_status_code);
+		})
 	}
 });
--- components/comp_pro/factory.js
+++ components/comp_pro/factory.js
@@ -1,5 +1,45 @@
 tappyn.factory('comproFactory', function($http){
 	var fact = {};
 
+	fact.grabProfile = function(id){
+		return $http({
+			method : 'GET',
+			url : 'index.php/companies/show/'+id,
+			headers : {
+				'Content-type' : 'application/x-www-form-urlencoded'
+			}
+		});
+	}
+
+	fact.requestContest = function(id){
+		return $http({
+			method : 'POST',
+			url : 'index.php/companies/request_contest/'+id,
+			headers : {
+				'Content-type' : 'application/x-www-form-urlencoded'
+			}
+		});
+	}
+
+	fact.followCompany = function(id){
+		return $http({
+			method : 'POST',
+			url : 'index.php/user/follow/'+id,
+			headers : {
+				'Content-type' : 'application/x-www-form-urlencoded'
+			}
+		});
+	}
+
+	fact.unfollowCompany = function(id){
+		return $http({
+			method : 'POST',
+			url : 'index.php/user/unfollow/'+id,
+			headers : {
+				'Content-type' : 'application/x-www-form-urlencoded'
+			}
+		});
+	}
+
 	return fact;
 })
