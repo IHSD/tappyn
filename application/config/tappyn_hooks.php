f1a6dd253f5d83cf6ac3946d684c6b43fc62fd23
Robert Wittman <rob@ihsdigital.com>
2016-04-11 11:36:48 -0400

    Removed admin folder

--- application/config/tappyn_hooks.php
+++ application/config/tappyn_hooks.php
@@ -1,7 +1,19 @@
 <?php defined("BASEPATH") or exit('No direct script access allowed');
 
+/**
+ * Setting hooks for triggered application events
+ *
+ * $config['hooks'] = array(
+ * 		'controller' => array(
+ * 			'event' => "Event that should trigger the hook",
+ * 			'name'  => "Name of the hook, just for namespacing",
+ * 			'class' => "Name of the class to call",
+ * 			'method' => "Method to call",
+ * 			'args' => "Associative array of arguments to pass"
+ * 		)
+ * )
+ */
 $config['hooks'] = array();
-
 
 $config['hooks']['test'] = array();
 $config['hooks']['test'][] = [
@@ -10,4 +22,12 @@ $config['hooks']['test'][] = [
     'class' => 'test_library',
     'method' => 'greet',
     'args' => array('testarino')
+];
+$config['hooks']['auth'] = array();
+$config['hooks']['auth'][] = [
+    'event' => 'user_registration',
+    'name' => 'send_user_verification_email',
+    'class' => 'mailer',
+    'method' => 'queue',
+    'args' => array()
 ];
