ad3dbd8a8685d364f4a880a33f072f0c3b95b4b1
Robert Wittman <rob@ihsdigital.com>
2016-04-13 13:52:19 -0400

    Finished explicit declaration of routes

--- api/v1/application/config/routes.php
+++ api/v1/application/config/routes.php
@@ -1,95 +1,101 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-/*
-| -------------------------------------------------------------------------
-| URI ROUTING
-| -------------------------------------------------------------------------
-| This file lets you re-map URI requests to specific controller functions.
-|
-| Typically there is a one-to-one relationship between a URL string
-| and its corresponding controller class/method. The segments in a
-| URL normally follow this pattern:
-|
-|	example.com/class/method/id/
-|
-| In some instances, however, you may want to remap this relationship
-| so that a different class/function is called than the one
-| corresponding to the URL.
-|
-| Please see the user guide for complete details:
-|
-|	http://codeigniter.com/user_guide/general/routing.html
-|
-| -------------------------------------------------------------------------
-| RESERVED ROUTES
-| -------------------------------------------------------------------------
-|
-| There are three reserved routes:
-|
-|	$route['default_controller'] = 'welcome';
-|
-| This route indicates which controller class should be loaded if the
-| URI contains no data. In the above example, the "welcome" class
-| would be loaded.
-|
-|	$route['404_override'] = 'errors/page_missing';
-|
-| This route will tell the Router which controller/method to use if those
-| provided in the URL cannot be matched to a valid route.
-|
-|	$route['translate_uri_dashes'] = FALSE;
-|
-| This is not exactly a route, but allows you to automatically route
-| controller and method names that contain dashes. '-' isn't a valid
-| class or method name character, so it requires translation.
-| When you set this option to TRUE, it will replace ALL dashes in the
-| controller and method URI segments.
-|
-| Examples:	my-controller/index	-> my_controller/index
-|		my-controller/my-method	-> my_controller/my_method
-*/
+/*========================
+  Globals
+========================*/
 $route['default_controller'] = 'errors/no_route';
 $route['404_override'] = 'errors/show_404';
 $route['translate_uri_dashes'] = FALSE;
 
-/**
- * Authentication
- */
-$route['signup'] = 'auth/create_user';
-$route['login'] = 'auth/login';
-$route['logout'] = 'auth/logout';
-$route['forgot_password'] = 'auth/forgot_password';
-/**
- * Base Routes
- */
-$route['profile'] = 'users/profile';
-$route['how_it_works'] = 'welcome/how_it_works';
-$route['contact'] = 'welcome/contact_us';
-$route['tos'] = 'welcome/tos';
-$route['privacy_policy'] = 'welcome/privacy_policy';
-$route['faq'] = 'welcome/faq';
-$route['mailing_list']['post'] = 'welcome/mailing_list';
-$route['unsubscribe']['get'] = 'welcome/unsubscribe';
-$route['dashboard'] = 'users/dashboard';
-$route['in_progress'] = 'users/in_progress';
-$route['completed'] = 'users/completed';
-$route['uploads/(:any)'] = 'uploads/index/$1';
+/*========================
+  Authentication
+========================*/
+$route['login']['post']                 = 'auth/login';
+$route['logout']['get']                 = 'auth/logout';
+$route['is_logged_in']['get']           = 'auth/is_logged_in';
+$route['facebook']['get']               = 'auth/facebook';
+$route['password']['post']              = 'auth/change_password';
+$route['forgot_password']['post']       = 'auth/forgot_password';
+$route['reset_password']['post']        = 'auth/reset_password';
+$route['signup']['post']                = 'auth/create_user';
+
+/*========================
+  Users
+========================*/
+$route['dashboard']['get']                  = 'users/dashboard';
+$route['profile']['get']                    = 'users/profile';
+$route['profile']['post']                   = 'users/profile';
+$route['stats']['get']                      = 'users/stats';
+$route['upvoted']['get']                    = 'users/upvoted';
+$route['companies/(:num)/follow']['post']   = 'users/follow/$1';
+$route['companies/(:num)/unfollow']['post'] = 'users/unfollow/$1';
+$route['interests/(:num)/add']['post']      = 'interests/add/$1';
+$route['interests/(:num)/remove']['post']   = 'interests/remove/$1';
+
+/*========================
+  User Accounts
+========================*/
+$route['accounts/details']['get']           = 'accounts/details';
+$route['accounts/details']['post']          = 'accounts/details';
+$route['accounts/payment_methods']['post']  = 'accounts/payment_methods';
+$route['accounts/remove_method']['post']    = 'accounts/remove_method';
+$route['accounts/default_method']['post']   = 'accounts/default_method';
+
+/*========================
+  Company Accounts
+========================*/
+$route['companies/accounts']['post']        = 'companies/accounts';
+$route['companies/remove_method']['post']   = 'companies/removeCard';
+$route['companies/payment/(:num)']['post']  = 'companies/payment/$1';
+$route['companies/default_method']['post']  = 'companies/setAsDefault';
+
+/*========================
+  Contests
+========================*/
+$route['contests/start_dates']['get']       = 'contests/start_dates';
+$route['contests']['get']                   = 'contests/index';
+$route['contests/leaderboard']['get']       = 'contests/leaderboard';
+$route['contests/(:num)']['get']            = 'contests/show/$1';
+$route['contests/(:num)/winner']['get']     = 'contests/winner/$1';
+$route['contests']['post']                  = 'contests/create';
+$route['contests/(:num)']['post']           = 'contests/update/$1';
+$route['contests/(:num)/winner']['post']    = 'contests/select_winner/$1';
+$route['contests/(:num)/delete']['post']    = 'contests/delete/$1';
+
+/*========================
+  Submissions
+========================*/
+$route['contests/(:num)/submissions']['post']   = 'submissions/create/$1';
+$route['submissions/leaderboard']['get']        = 'submissions/leaderboard';
+$route['submissions/(:num)/shares']['post']     = 'submissions/share/$1';
+$route['submisisons/(num)/rating']['post']      = 'submissions/rating/$1';
+$route['submissions/(:num)/votes']['post']      = 'votes/create/$1';
+
+/*========================
+  Notifications
+========================*/
+$route['notifications']['get']              = 'notifications/index';
+$route['notifications/unread']['get']       = 'notifications/unread';
+$route['notifications/read']['post']        = 'notifications/read';
+
+/*========================
+  Payouts
+========================*/
+$route['payouts']['get']                    = 'payouts/index';
+$route['payouts/(:num)']['get']             = 'payouts/show/$1';
+$route['payouts/(:num)/claim']['post']      = 'payouts/claim/$1';
+
+/*========================
+  Vouchers
+========================*/
+$route['vouchers']['get']                   = 'vouchers/is_valid';
 
 /**
- * Contests
+ * Anything that does not match a defined route, we send to the 404 of our
+ * error controller. TThis forces routes to match explicitly
  */
-$route['contests/(:num)'] = 'contests/show/$1';
-$route['submissions/(:num)'] = 'submissions/index/$1';
-$route['contests/(:num)/submissions'] = 'contests/submissions/$1';
-
-/**
- * Submissions
- */
-$route['contests/(:num)/submissions'] = 'submissions/index/$1';
-
-
-/**
- * Admin
- */
-$route['admin']['get'] = 'admin/home/index';
+$route['(:any)/(:any)/(:any)/(:any)']       = 'errors/show_404';
+$route['(:any)/(:any)/(:any)']              = 'errors/show_404';
+$route['(:any)/(:any)']                     = 'errors/show_404';
+$route['(:any)']                            = 'errors/show_404';
