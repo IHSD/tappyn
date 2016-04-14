54c865bac385b31dcb046959b201eff175f4c415
Robert Wittman <rob@ihsdigital.com>
2016-04-14 15:21:51 -0400

    Fixed something

--- api/v1/application/core/MY_Controller.php
+++ api/v1/application/core/MY_Controller.php
@@ -42,8 +42,7 @@ class MY_Controller extends CI_Controller
         }
         Hook::trigger('hook_initialize');
 
-        $this->config->load('secrets', TRUE)
-        echo $this->config->item('stripe_api_key', 'secrets');
+        $this->config->load('secrets', TRUE);
         \Stripe\Stripe::setApiKey($this->config->item('stripe_api_key', 'secrets'));
     }
 }
