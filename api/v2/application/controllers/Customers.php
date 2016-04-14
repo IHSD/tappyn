583fa682714c76747e340365866b8c21d4edd5e0
Robert Wittman <rob@ihsdigital.com>
2016-04-14 12:21:03 -0400

    Updated changes

---
+++ api/v2/application/controllers/Customers.php
@@ -0,0 +1,9 @@
+<?php defined("BASEPATH") or exit('No direct script access allowed');
+
+class Customers extends MY_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
