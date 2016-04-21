0e88da9e4501a24b037afcf9d2950af60b769c3d
Robert Wittman <rob@ihsdigital.com>
2016-04-21 16:19:05 -0400

    Replaced bucket limit

--- api/v1/application/controllers/Amazon.php
+++ api/v1/application/controllers/Amazon.php
@@ -28,6 +28,7 @@ class Amazon extends CI_Controller
                 {"bucket": "' . $this->bucket_name . '"},
                 ["starts-with", "$key", ""],
                 {"acl": "public-read"},
+                ["content-length-range", 0, 10485760],
                 ["starts-with", "$Content-Type", ""]
             ]
         }';
