8f7fc8ae58cc22d9ff38676b19fc091300992947
Robert Wittman <rob@ihsdigital.com>
2016-04-25 15:48:09 -0400

    Added image functionality

---
+++ api/v1/application/libraries/Image.php
@@ -0,0 +1,33 @@
+<?php defined("BASEPATH") or exit('No direct script access allowed');
+
+class Image
+{
+    public function __construct()
+    {
+
+    }
+
+    public function __get($var)
+    {
+        return get_instance()->$var;
+    }
+
+    public function upload($upload, $filename)
+    {
+        if($this->s3->upload($upload, $filename))
+        {
+            return TRUE;
+        }
+        return FALSE;
+    }
+
+    public function compress($base64_data)
+    {
+
+    }
+
+    public function create_thumbnail()
+    {
+
+    }
+}
