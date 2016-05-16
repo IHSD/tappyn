da9dab4a6692ba692d838dfe76587b3b767c1432
Robert Wittman <rob@ihsdigital.com>
2016-05-16 11:04:10 -0400

    Restyled contest_completed email

--- api/v1/application/views/email_templates/contest_completed.php
+++ api/v1/application/views/email_templates/contest_completed.php
@@ -16,16 +16,16 @@ $query_string['redirect'] = 'dashboard';
 
 <!-- Start Email Content -->
 
-<p>Hi <?php echo $company->name; ?>,</p><br>
+<p style='text-align:center;margin:auto;min-width:450px;width:50%'>Hi <?php echo $company->name; ?>,</p><br>
 
-<p>We hope you’re doing well!</p><br>
+<p style='text-align:center;margin:auto;min-width:450px;width:50%'>We hope you’re doing well!</p><br>
 
-<p>This is a friendly reminder that your contest recently closed and it’s time to select your favorite submission as a winner.</p><br>
+<p style='text-align:center;margin:auto;min-width:450px;width:50%'>This is a friendly reminder that your contest recently closed and it’s time to select your favorite submission as a winner.</p><br>
 
-<p>Writers in your contest have been anxiously waiting to find out if they have won. If you have questions, let us know. If you’re ready, select the winner
+<p style='text-align:center;margin:auto;min-width:450px;width:50%'>Writers in your contest have been anxiously waiting to find out if they have won. If you have questions, let us know. If you’re ready, select the winner
 Below!</p><br>
 
-<p>Please note that if you don’t select your winner soon, the submission with the most upvotes will be announced the winner.. So don’t delay: select your favorite submission now, and keep the ball rolling!</p><br>
+<p style='text-align:center;margin:auto;min-width:450px;width:50%'>Please note that if you don’t select your winner soon, the submission with the most upvotes will be announced the winner.. So don’t delay: select your favorite submission now, and keep the ball rolling!</p><br>
 
 <!-- End Email Content -->
 
