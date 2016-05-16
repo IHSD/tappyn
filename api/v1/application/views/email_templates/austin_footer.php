da5b1f27c48186b3cf4260c8e94c0d4aca0acebd
Robert Wittman <rob@ihsdigital.com>
2016-05-16 10:30:02 -0400

    Added new footer

---
+++ api/v1/application/views/email_templates/austin_footer.php
@@ -0,0 +1,12 @@
+<?php defined("BASEPATH") or exit('No direct script access allowed'); ?>
+
+
+<p style='margin:auto;width:50%;'>
+    Thanks fo using Tappyn,
+</p>
+<br>
+<p style='margin:auto;width:50%;'>
+    Austin
+    <br>
+    Co-Founder - Tappyn <a href="<?php echo base_url(); ?>">www.tappyn.com</a>
+</p>
