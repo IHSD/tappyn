515ef5ea1f49862887cf7aa07b866963c4f2d665
amatthi <austin.matthiessen@gmail.com>
2016-06-02 12:44:01 -0400

    email lang fix

--- public/api/v1/application/views/email_templates/winner_announced.php
+++ public/api/v1/application/views/email_templates/winner_announced.php
@@ -17,18 +17,24 @@ $query_string['redirect'] = 'dashboard';
 
 <!-- Start Email Content -->
 
-<h5 style='text-align:center'><?php echo $company->name; ?>'s contest has ended</h5>
+<h2 style='text-align:center;margin:auto;min-width:450px;width:50%'><?php echo $company->name; ?>'s contest has ended</h2>
+<br>
+<!-- Orange header -->
+<p style='text-align:center;margin:auto;min-width:450px;width:50%;border-bottom:2px solid #FF5E00'></p><br>
 
 <p style='text-align:left;margin:auto;width:600px'>They loved all of the submissions, but there was just one that edged out the rest.</p>
 
 <?php $query_string['redirect'] = 'contest/'.$contest->id; ?>
-<p style='text-align:left;margin:auto;width:600px'>See the winning submission <?php echo anchor('api/v1/analytics/click?'.http_build_query($query_string), 'here'); ?><br/></p>
-
+<p style='text-align:left;margin:auto;width:600px'>See the winning submission <?php echo anchor('api/v1/analytics/click?'.http_build_query($query_string), 'here'); ?></p>
+<br>
 <?php $query_string['redirect'] = 'guide'; ?>
-<p style='text-align:left;margin:auto;width:600px'>Wondering why your submission wasn't chosen? Take a look at our creative guide to learn more about what makes a great ad <?php echo anchor('api/v1/analytics/click?'.http_build_query($query_string), 'here'); ?><br/></p>
-
+<p style='text-align:left;margin:auto;width:600px'>Wondering why your submission wasn't chosen? Take a look at our creative guide to learn more about what makes a great ad <?php echo anchor('api/v1/analytics/click?'.http_build_query($query_string), 'here'); ?></p>
+<br>
 <?php $query_string['redirect'] = 'contests'; ?>
 <p style='text-align:left;margin:auto;width:600px'>Or you can see some of our <?php echo anchor('api/v1/analytics/click?'.http_build_query($query_string), 'new contests'); ?><br/></p>
+<br>
+<!-- Orange header -->
+<p style='text-align:center;margin:auto;min-width:450px;width:50%;border-bottom:2px solid #FF5E00'></p><br>
 
 <!-- End Email Content -->
 
