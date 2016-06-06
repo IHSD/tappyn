ca3fd98e60241eb12bc64fd8727d8bfd58f77752
Onion Lian <onininon@dejavu.studio>
2016-06-06 23:56:14 +0800

    fix sql bug

--- public/api/v1/application/controllers/Submissions.php
+++ public/api/v1/application/controllers/Submissions.php
@@ -57,8 +57,8 @@ class Submissions extends CI_Controller
          */
         $usub = false;
         foreach ($submissions as $key => $submission) {
-            $submissions[$key]->owner = $this->db->select('first_name, last_name')->from('users')->where('id', $submission->owner)->limit(1)->get()->row();
             $submissions[$key]->avatar_url = $this->db->select('avatar_url')->from('profiles')->where('id', $submission->owner)->limit(1)->get()->row()->avatar_url;
+            $submissions[$key]->owner = $this->db->select('first_name, last_name')->from('users')->where('id', $submission->owner)->limit(1)->get()->row();
             if ($this->ion_auth->logged_in()) {
                 if ($submission->owner == $this->ion_auth->user()->row()->id) {
                     $submissions[$key]->user_owned = true;
