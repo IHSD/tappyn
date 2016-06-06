75094436a1c7a2adbac9da00fb4267d889ff88e2
amatthi <austin.matthiessen@gmail.com>
2016-06-06 15:59:02 -0400

    comp dash img

--- public/components/dashboard/controller.js
+++ public/components/dashboard/controller.js
@@ -1,4 +1,35 @@
-tappyn.controller('dashController', function($scope, $rootScope, $route, dashFactory){
+tappyn.controller('dashController', function($scope, $rootScope, $route, dashFactory, $upload){
+
+	$scope.logged_in();
+	$scope.amazon_connect('tappyn');
+	$scope.states = profileModel.states;
+
+	$scope.select_file = function($files, upload_field_to) {
+					var file = $files[0];
+					var url = APP_ENV.amazon_aws_url;
+					var new_name = Date.now();
+					upload_field_to = (typeof upload_field_to == 'undefined') ? 'logo_url' : upload_field_to;
+					$upload.upload({
+							url: url,
+							method: 'POST',
+							data: {
+									key: new_name,
+									acl: 'public-read',
+									"Content-Type": file.type === null || file.type === '' ?
+											'application/octet-stream' : file.type,
+									AWSAccessKeyId: $rootScope.key.key,
+									policy: $rootScope.key.policy,
+									signature: $rootScope.key.signature
+							},
+							file: file,
+					}).success(function() {
+							if (upload_field_to == 'avatar_url') {
+									$scope.profile.avatar_url = url + new_name;
+							} else {
+									$scope.profile.logo_url = url + new_name;
+							}
+					});
+			}
 	//on page load grab all
 	$scope.type = '';
 	$scope.adding_payment = {show : false, id : ''};
