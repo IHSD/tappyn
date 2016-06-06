b66e89262af75a4f1759c89620292eb388e70999
amatthi <austin.matthiessen@gmail.com>
2016-06-06 18:51:25 -0400

    test merge

---
+++ public/components/dashboard/footer.php
@@ -0,0 +1,15 @@
+<?php defined("BASEPATH") or exit('No direct script access allowed'); ?>
+
+
+<p style='text-align:left;margin:auto;width:600px'>
+    Feel free to hit me up with any feedback or questions!
+</p>
+<br>
+<p style='text-align:left;margin:auto;width:600px'>
+  -Alek
+  <br>
+  Co-Founder Tappyn
+    <br>
+  (678)-367-1060
+    <a href="<?php echo base_url(); ?>">www.tappyn.com</a>
+</p>
