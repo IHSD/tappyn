f50549a7bd2f5c67f810359957266846eb2fe9f7
amatthi <amatthi@emory.edu>
2016-06-24 16:48:35 -0400

    location path contests test

--- public/components/config.js
+++ public/components/config.js
@@ -587,7 +587,7 @@ tappyn.controller("ApplicationController", function($scope, $rootScope, $upload,
                         eventAction: 'Signup',
                         eventLabel: 'New User Email'
                     });
-                    if ($scope.registration.type != "company") $route.reload();
+                    if ($scope.registration.type != "company") $location.path("/contests");;
                     $scope.registration = { show: false, type: '', object: '' };
                 } else $scope.set_alert(response.message, "default");
             } else if (response.http_status_code == 500) $scope.set_alert(response.error, "error");
