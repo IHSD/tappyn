00ebe7e83762ac3139b995e4aa9ce44b0af2bce1
amatthi <amatthi@emory.edu>
2016-06-27 11:25:58 -0400

    ended controller update

--- public/components/ended/controller.js
+++ public/components/ended/controller.js
@@ -2,6 +2,7 @@ tappyn.controller("endedController", function($scope, $location, $routeParams, e
 	endedFactory.grabContest($routeParams.id).success(function(response){
 		$scope.contest = response.data.contest;
 		$scope.winner = response.data.winner;
+		$scope.submissions = response.data.submissions;
 		if($scope.contest.status == 'active') $location.path('/contest/'+$routeParams.id);
 	})
 })
