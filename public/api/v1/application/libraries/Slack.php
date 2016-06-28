bc2438ebad79acd3af61ba5e146f84701be6d65b
Onion Lian <onininon@dejavu.studio>
2016-06-28 15:12:41 +0800

    fix slack

--- public/api/v1/application/libraries/Slack.php
+++ public/api/v1/application/libraries/Slack.php
@@ -1,7 +1,6 @@
 <?php if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
-use Maknz\Slack\Client;
 
 class slack
 {
@@ -9,7 +8,7 @@ class slack
     public function __construct()
     {
         // setting from https://my.slack.com/services/new/incoming-webhook
-        $this->client = new Client('https://hooks.slack.com/services/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');
+        $this->client = new Maknz\Slack\Client('https://hooks.slack.com/services/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');
     }
 
     public function send($msg = '')
