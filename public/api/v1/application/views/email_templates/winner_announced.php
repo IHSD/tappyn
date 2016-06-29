b562ec2a20f699d1e73bd2ce8698b72cd16795e3
amatthi <amatthi@emory.edu>
2016-06-29 11:21:05 -0400

    email edits

--- public/api/v1/application/views/email_templates/winner_announced.php
+++ public/api/v1/application/views/email_templates/winner_announced.php
@@ -21,7 +21,7 @@ $query_string['redirect'] = 'dashboard';
 <p style='text-align:center;margin:auto;min-width:450px;width:50%;border-bottom:2px solid #FF5E00'></p><br>
 
 <?php $query_string['redirect'] = 'ended/' . $contest->id;?>
-<p style='text-align:left;margin:auto;width:600px'>See all of the purchased ads <?php echo anchor('api/v1/analytics/click?' . http_build_query($query_string), 'here'); ?>.</p>
+<p style='text-align:left;margin:auto;width:600px'>Check out your ad's Click Through Rate and all of the Tapped ads <?php echo anchor('api/v1/analytics/click?' . http_build_query($query_string), 'here'); ?>.</p>
 <br>
 <?php $query_string['redirect'] = 'guide';?>
 <p style='text-align:left;margin:auto;width:600px'>Wondering why your ad wasn't Tapped? Take a look at our creative guide to learn more about what makes a great ad <?php echo anchor('api/v1/analytics/click?' . http_build_query($query_string), 'here'); ?>.</p>
