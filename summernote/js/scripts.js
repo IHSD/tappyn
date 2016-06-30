f27adfdd996435bd319781167958ea7a2261cffb
amatthi <amatthi@emory.edu>
2016-06-29 20:21:53 -0400

    summernote try

---
+++ summernote/js/scripts.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){//Summernote editor snippet - js
+
+$.getScript('//cdnjs.cloudflare.com/ajax/libs/summernote/0.5.1/summernote.min.js',function(){
+  $('#summernote').summernote();
+});
+
+});
