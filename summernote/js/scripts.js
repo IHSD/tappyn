1e774ab485acb28b03fd7170becf1a502a008e4c
amatthi <amatthi@emory.edu>
2016-06-29 20:23:51 -0400

    summernote delete

--- summernote/js/scripts.js
+++
@@ -1,8 +0,0 @@
-
-$(document).ready(function(){//Summernote editor snippet - js
-
-$.getScript('//cdnjs.cloudflare.com/ajax/libs/summernote/0.5.1/summernote.min.js',function(){
-  $('#summernote').summernote();
-});
-
-});
