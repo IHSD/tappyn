699247c4c791ac9e465bc35eb899737f47689f1a
amatthi <amatthi@emory.edu>
2016-07-03 13:58:37 -0400

    hide div

--- public/components/dashboard/controller.js
+++ public/components/dashboard/controller.js
@@ -48,7 +48,7 @@ tappyn.controller('dashController', function($scope, $rootScope, $route, dashFac
 
     $scope.back_table = function() {
         $scope.view = 'table';
-        contest.status = null;
+        $scope.contest.status = null;
     }
 
 
