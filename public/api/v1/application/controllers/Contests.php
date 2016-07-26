15f19fa47d968b37792a17713b83b2b368b21d91
amatthi <amatthi@emory.edu>
2016-07-25 21:49:58 -0400

    objective field not required

--- public/api/v1/application/controllers/Contests.php
+++ public/api/v1/application/controllers/Contests.php
@@ -206,7 +206,7 @@ class Contests extends CI_Controller
         }
 
         $this->form_validation->set_rules('different', 'How Your Different', 'required');
-        $this->form_validation->set_rules('objective', 'Objective', 'required');
+        //$this->form_validation->set_rules('objective', 'Objective', 'required');
         $this->form_validation->set_rules('platform', 'Format', 'required');
         $this->form_validation->set_rules('summary', 'Summary', 'required');
         //$this->form_validation->set_rules('attachment', 'Photo', 'required');
