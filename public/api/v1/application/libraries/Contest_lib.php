62625a9120d3f4d53a4a20248cf8fc8209d4e12a
amatthi <amatthi@emory.edu>
2016-07-26 21:34:02 -0400

    design edits

--- public/api/v1/application/libraries/Contest_lib.php
+++ public/api/v1/application/libraries/Contest_lib.php
@@ -27,7 +27,7 @@ class Contest_lib
         }
         $sub = $this->subscription_lib->get_by_user_id($this->ion_auth->user()->row()->id);
         if ($sub['can_launch'] == false) {
-            return 'You need upgrate subscription to launch';
+            return 'You need to upgrade your current subscription to launch';
         }
         $start_time = date('Y-m-d H:i:s');
         $stop_time  = date('Y-m-d H:i:s', strtotime('+7 days'));
