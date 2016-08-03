35c022da26b44de8c75b1a4aba8f103354e5ad97
amatthi <amatthi@emory.edu>
2016-08-03 11:53:50 -0700

    delete

--- public/components/contest/factory.js
+++ public/components/contest/factory.js
@@ -11,16 +11,6 @@ tappyn.factory('contestFactory', function($http){
 		});
 	}
 
-	fact.grabTotals = function() {
-        return $http({
-            method: 'GET',
-            url: 'api/v1/stats',
-            headers: {
-                'Content-type': 'application/x-www-form-urlencoded'
-            }
-        });
-    }
-
 	fact.submitTo = function(id, submission){
 		return $http({
 			method : 'POST',
